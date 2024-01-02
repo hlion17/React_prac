54895065e87a5c9557c3d26fba4048da463fff86
hlion17 <yourdad20626@gmail.com>
2024-01-02 12:12:56 +0900

    delete: react memo example

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import {useEffect, useMemo, useRef, useState} from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -63,7 +62,6 @@ function App() {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체일기: {data.length}</div>
       <div>기분좋은 일기 개수: {goodCount}</div>
